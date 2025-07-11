cffa03b8674783f3435b3e84d82fa8a6c5d18cdf
Metal Whale <metalwhale.dev@gmail.com>
2025-07-11 12:27:03 +0000

    Train a minimal DDPM.

--- minimal_ddpm/main.py
+++ minimal_ddpm/main.py
@@ -1,5 +1,19 @@
+import os
+import pathlib
+
+from minimal_ddpm.data import MixtureGaussian
+from minimal_ddpm.model import MinimalDdpm, train
+
+
+STORAGE_DIR = pathlib.Path(__file__).parent.parent / "storage"
+
+
 def main():
-    print("Hello from minimal-ddpm!")
+    target_distribution = MixtureGaussian([(-2.0, 1.0, 0.4), (2.0, 0.5, 0.6)])
+    model = MinimalDdpm()
+    result_dir = STORAGE_DIR / "minimal_ddpm"
+    os.makedirs(result_dir, exist_ok=True)
+    train(result_dir, target_distribution, model)
 
 
 if __name__ == "__main__":
---
+++ minimal_ddpm/minimal_ddpm/data.py
@@ -0,0 +1,51 @@
+import io
+
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+
+
+class MixtureGaussian:
+    _parameters: list[tuple[float, float]]
+    _weights: list[float]
+
+    def __init__(self, parameters: list[tuple[float, float, float]]):
+        self._parameters = []
+        self._weights = []
+        assert isinstance(parameters, list)
+        for parameter in parameters:
+            assert isinstance(parameter, tuple)
+            assert len(parameter) == 3
+            m, s, w = parameter  # Mean, standard deviation and weight
+            assert isinstance(m, float) and isinstance(s, float)
+            self._parameters.append((m, s))
+            assert isinstance(w, float)
+            self._weights.append(w)
+        assert sum(self._weights) == 1.0
+
+    def sample(self, size: int) -> list[float]:
+        samples = []
+        for _ in range(size):
+            parameter = self._parameters[np.random.choice(len(self._parameters), p=self._weights)]
+            m, s = parameter
+            samples.append(np.random.normal(loc=m, scale=s))
+        return samples
+
+
+def plot_histogram(
+    samples: list[float],
+    bin_num: int,
+    domain: tuple[float, float] | None = None,
+    top: float | None = None,
+) -> np.ndarray:
+    if domain is None:
+        domain = (min(samples), max(samples))
+    plt.hist(samples, bins=bin_num, range=domain)
+    if top is not None:
+        plt.ylim(top=top)
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format="png")
+    plt.clf()
+    buffer.seek(0)
+    image = np.array(Image.open(buffer))
+    return image
